d065081f18e22a68af1892e6ecc52acd22ba4a98
Meghane Decroocq <meghdecroocq@gmail.com>
2016-12-16 18:07:24 +0100

    Ajout fichier Noeud.cpp

---
+++ Noeud.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include"Noeud.h"
+
+class Noeud{
+
+  Noeud:: Noeud(int* cle){
+    this<-cle=cle;
+	Noeud* fg=NULL;
+	Noeud* fd=NULL;
+  }
+  int* Noeud:: Get_cle(){
+	return cle;
+
+  }
+  Noeud* Noeud:: Get_fg(){
+    return fg;
+  }
+  Noeud* Noeud:: Get_fd(){
+    return fd;
+  }
+  void Noeud:: Set_cle(int*a){
+    cle=a;
+  }
+  void Noeud:: Set_fg(Noeud* a){
+    fg=a;
+  }
+  void Noeud:: Set_fd(Noeud* a){
+    fd=a;
+  }
+
+  Noeud Noeud:: new_nd(int* a){
+    Noeud objet(a);
+
+  }
