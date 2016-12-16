55daa15fcabace02adc9753e539cce7c5efc1cce
Paul de Nailly <pdenailly@bs406-s16-23.insa-lyon.fr>
2016-12-16 18:11:02 +0100

    Ajout main.cpp

---
+++ Bureau/C++/TD_5/main.cpp
@@ -0,0 +1,12 @@
+#include "Noeud.h"
+#include <stdio.h>
+
+int main(){
+  int a = 5;
+  Noeud liste[10];
+  liste_g[1]=Noeud objet(a);
+  for(int k=2; k<11;k++)
+    {
+      liste_g[k]=Noeud objet(liste[k-1].get_fg()); 
+      objet.new_ng();
+  
