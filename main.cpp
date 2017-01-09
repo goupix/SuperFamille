8b491ca7c4eacff1b3c2121117cec8ffa8b3845d
Aurelien Colas <acolas@bs406-s16-22.insa-lyon.fr>
2017-01-09 17:09:28 +0100

    fonction recher

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include<iostream>
 #include"Noeud.h"
+
 using namespace std;
 
-void creation(){
+Noeud creation(){
   int* cle=NULL;
   Noeud arbre_vide(cle);
-  cout<<arbre_vide.Get_cle()<<endl;
+  return arbre_vide;
 }
 
-
-
-
+Noeud* recherche(int* r,Noeud p){
+  Noeud* pN = &p;
+  Noeud N(NULL);
+  bool t = false; 
+  while(r==false){
+    N=*pN;
+    if(*(N.Get_cle())==*r){
+      t=true;
+      cout<<"Le noeud qu'on recherche existe et a été atteint."<<endl;
+      return pN;
+    } else if((*(N.Get_cle())<*r)and(N.Get_fg()!=NULL)){
+      pN=N.Get_fg();
+    } else if((*(N.Get_cle())>*r)and(N.Get_fd()!=NULL)){
+      pN=N.Get_fd();
+    } else {
+      t=true;
+      cout<<"Le noeud qu'on recherche n'existe pas, le plus proche a été renvoye."<<endl;
+      return pN;
+    }
+  }
+}
 
 int main(){
-  creation();
-  cout<<"coucou"<<endl;
+  Noeud pere=creation();
   
 }
